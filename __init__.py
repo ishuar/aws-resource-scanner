9988fedf57d3d575d0df19bd97c8ebc3a7b68cb4
ishuar <ishansharma887@gmail.com>
2025-09-24 02:53:21 +0200

    chore: fix author name & `isort`
    Signed-off-by: ishuar <ishansharma887@gmail.com>

--- __init__.py
+++ __init__.py
@@ -2,10 +2,6 @@
 AWS Service Scanner Package
 """
 
-from .aws_scanner import main
-
 __version__ = "1.0.0"
-__author__ = "Ishan Sharma"
+__author__ = "ishuar"
 __email__ = "ishansharma887@gmail.com"
-
-__all__ = ["main"]
--- aws_scanner_lib/__init__.py
+++ aws_scanner_lib/__init__.py
@@ -5,12 +5,12 @@ This package contains modular components for the AWS Service Scanner tool.
 """
 
 __version__ = "1.0.0"
-__author__ = "AWS Scanner Team"
+__author__ = "ishuar"
 
 # Import commonly used functions for convenience
-from .cache import get_cache_key, get_cached_result, cache_result
-from .scan import scan_service, scan_region
-from .outputs import generate_markdown_summary, output_results, compare_with_existing
+from .cache import cache_result, get_cache_key, get_cached_result
+from .outputs import compare_with_existing, generate_markdown_summary, output_results
+from .scan import scan_region, scan_service
 
 __all__ = [
     "get_cache_key",
