e6ffe958e2f91457288c8d01a03fc7425e987ab8
ishuar <ishansharma887@gmail.com>
2025-09-25 17:23:44 +0200

    chore: fmt
    Signed-off-by: ishuar <ishansharma887@gmail.com>

--- services/__init__.py
+++ services/__init__.py
@@ -6,12 +6,12 @@ This package contains modules for scanning different AWS services.
 Each service has its own module with a scan function and output processing.
 """
 
-from .ec2_service import scan_ec2, process_ec2_output
-from .s3_service import scan_s3, process_s3_output
-from .ecs_service import scan_ecs, process_ecs_output
-from .elb_service import scan_elb, process_elb_output
-from .vpc_service import scan_vpc, process_vpc_output
-from .autoscaling_service import scan_autoscaling, process_autoscaling_output
+from .autoscaling_service import process_autoscaling_output, scan_autoscaling
+from .ec2_service import process_ec2_output, scan_ec2
+from .ecs_service import process_ecs_output, scan_ecs
+from .elb_service import process_elb_output, scan_elb
+from .s3_service import process_s3_output, scan_s3
+from .vpc_service import process_vpc_output, scan_vpc
 
 __all__ = [
     "scan_ec2",
